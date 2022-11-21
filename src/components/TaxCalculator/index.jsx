93b5f51ba6962809db32ca7de0bc7d41d2557983
TheHerran <alexandreherran@outlook.com>
2022-11-21 00:44:44 -0300

    Feat: Layout and finalization of project

--- src/components/TaxCalculator/index.jsx
+++ src/components/TaxCalculator/index.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import DisplayTax from "../DisplayTax";
 import Form from "../Form";
+import "./style.css"
 
-function TaxCalculator() {
+function TaxCalculator({ setIsOpen }) {
     const [calcResult, setCalcResult] = useState({})
     return (
         <section className="calcBox">
-            <Form setCalcResult={setCalcResult} />
-            <DisplayTax calcResult={calcResult} />
+            <div className="calcBoxTitle">
+            <h2>Calcular Taxas</h2>
+            <button onClick={() => setIsOpen(false)}>X</button>
+            </div>
+            <div className="calcContent">
+                <Form setCalcResult={setCalcResult} />
+                <DisplayTax calcResult={calcResult} />
+            </div>
         </section>
     )
 }
